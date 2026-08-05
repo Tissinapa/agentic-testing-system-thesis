1e47c0d2456c8a085b6a2372d65a6a6ebea1ceaa
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2026-08-05 11:44:25 +0300

    INIT: first init for this thesis

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agentic-testing-thesis!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ testi.py
@@ -0,0 +1,5 @@
+def testi():
+    print("toimiii")
+
+if __name__ == "__main__":
+    testi()
